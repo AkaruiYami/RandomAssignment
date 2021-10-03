96f8865e16a9a23b0daec95d70960f1570584bf8
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2021-10-03 22:57:17 +0800

    rename file

---
+++ Name Main/fileA.py
@@ -0,0 +1,7 @@
+import fileB
+
+print("This is from File A!")
+print(f"From File A File={__name__}")
+
+if __name__ == '__main__':
+    print("File A!")
---
+++ Name Main/fileB.py
@@ -0,0 +1,5 @@
+print("This is from a File B!")
+print(f"From File B={__name__}")
+
+if __name__ == '__main__':
+    print("File B!")
