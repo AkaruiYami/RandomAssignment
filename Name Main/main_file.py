ecfaa30829fa661e33e1f3614fe6609a34372be1
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2021-10-03 23:08:09 +0800

    remove old file

--- Name Main/main_file.py
+++
@@ -1,5 +0,0 @@
-print("This is from main file!")
-print(f"From Main File={__name__}")
-
-if __name__ == '__main__':
-    print("Main File")
--- Name Main/second_file.py
+++
@@ -1,7 +0,0 @@
-import main_file
-
-print("This is from a second file!")
-print(f"From Second File={__name__}")
-
-if __name__ == '__main__':
-    print("2nd file is main file now?")
